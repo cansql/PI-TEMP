c4cc5e069f03625b67a3ca35f7668355c999d864
Can Şahin <75973367+cansahinv@users.noreply.github.com>
2025-07-14 01:21:15 +0300

    Create temperature.sh

---
+++ temperature.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pull colour file
+source <(curl -sSL https://cdn.elewon.net/beacol/color.sh)
+
+# Get temperature from vcgencmd output → e.g. temp=52.3'C
+TEMP_RAW=$(vcgencmd measure_temp)
+
+# Pull the number → 52.3
+TEMP_FLOAT=$(echo "$TEMP_RAW" | grep -oP '[0-9]+\.[0-9]+')
+
+# Discard decimal part, integer only → 52
+TEMP_INT=${TEMP_FLOAT%%.*}
+
+#  Make a comparison
+if [ "$TEMP_INT" -ge 60 ]; then
+    echo -e "${GRAY}Instant Temperature: ${TEMP_FLOAT}°C${RESET}"
+    echo -e "${RED}Beware of high temperatures!${RESET}"
+else
+    echo -e "${GRAY}Instant Temperature: ${TEMP_FLOAT}°C${RESET}"
+    echo -e "${BLUE}Your temperature is fine!${RESET}"
+fi
