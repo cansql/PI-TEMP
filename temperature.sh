9af360a2b4cd5fbaea5fb8d354b501d72712789c
Can Şahin <canserkan888@gmail.com>
2025-11-25 23:11:55 +0300

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pull colour file
-source <(curl -sSL https://gsi.nebliy.com/beacol/color.sh)
+source <(curl -sSL https://static.cansahin.tr/beacol/color.sh)
 
 # Get temperature from vcgencmd output → e.g. temp=52.3'C
 TEMP_RAW=$(vcgencmd measure_temp)
